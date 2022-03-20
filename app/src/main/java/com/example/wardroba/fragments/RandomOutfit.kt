0ed478613d7a5ec81beae918aab5727b7b21802f
ofir <davidof@sheridancollege.ca>
2022-03-19 21:58:30 -0400

    created AddAccessory.kt, MyWardrobe.kt, and RandomOutfit.kt fragments as well as nav_graph.xml

---
+++ app/src/main/java/com/example/wardroba/fragments/RandomOutfit.kt
@@ -0,0 +1,13 @@
+package com.example.wardroba.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.wardroba.R
+
+
+class RandomOutfit : Fragment() {
+
+}
