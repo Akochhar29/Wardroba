4303998a6a1fafba2ef1797fc16d92a31a126365
ofir <davidof@sheridancollege.ca>
2022-03-19 23:21:12 -0400

    designed the fragment_add_accessory.xml and fragment_my_wardrobe.xml fragments

--- app/src/main/java/com/example/wardroba/fragments/MyWardrobe.kt
+++ app/src/main/java/com/example/wardroba/fragments/MyWardrobe.kt
@@ -5,9 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.example.wardroba.R
+import com.example.wardroba.databinding.FragmentMyWardrobeBinding
 
 
 class MyWardrobe : Fragment() {
-
+    private var _binding: FragmentMyWardrobeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMyWardrobeBinding.inflate(inflater,container,false)
+        val view = binding.root
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.accessory_types,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.spnAccessoryType.adapter = adapter
+        }
+        return view
+    }
 }
