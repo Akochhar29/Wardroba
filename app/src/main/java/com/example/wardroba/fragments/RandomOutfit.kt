2c21ed0269c013ed3f61b5e94b838d99e8636c4d
ofir <davidof@sheridancollege.ca>
2022-03-20 00:06:17 -0400

    added inflater to RandomOutfit.kt

--- app/src/main/java/com/example/wardroba/fragments/RandomOutfit.kt
+++ app/src/main/java/com/example/wardroba/fragments/RandomOutfit.kt
@@ -6,8 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.wardroba.R
+import com.example.wardroba.databinding.FragmentMyWardrobeBinding
+import com.example.wardroba.databinding.FragmentRandomOutfitBinding
 
 
 class RandomOutfit : Fragment() {
-
+    private var _binding: FragmentRandomOutfitBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRandomOutfitBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
 }
