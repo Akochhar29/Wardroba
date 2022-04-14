55b0db6e1c312b22ebda8957b7a9f0e4b48ef38f
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2022-04-14 12:18:49 -0400

    Added a data class to be a conversion to a kotlin variable from a colour API response

---
+++ app/src/main/java/com/example/wardroba/api/ColourMatches.kt
@@ -0,0 +1,35 @@
+package com.example.wardroba.api
+import kotlinx.serialization.*
+
+@Serializable
+data class ColourMatches(
+    val mode: String,
+    val count: String,
+    val colors: List<ColourData>
+) {
+
+    data class ColourData(
+        val hex: HexData,
+        val rgb: RgbData,
+        val name: ColourName
+    )
+
+    data class HexData(
+        val value: String,
+        val clean: String
+    )
+
+    data class RgbData(
+        val r: Int,
+        val g: Int,
+        val b: Int,
+        val value: String
+    )
+
+    data class ColourName(
+        val value: String,
+        val closest_named_hex: String,
+        val exact_match_name: Boolean,
+        val distance: Int
+    )
+}
