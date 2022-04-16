aeecded218ef6b113a703147eab37a89b4200888
Akochhar29 <aks.kochhar@gmail.com>
2022-04-16 19:29:40 -0400

    Fixed btnMainColour. Had to use different kind of button for it to change colours

--- app/src/main/java/com/example/wardroba/fragments/ColourRecommendations.kt
+++ app/src/main/java/com/example/wardroba/fragments/ColourRecommendations.kt
@@ -1,5 +1,6 @@
 package com.example.wardroba.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListAdapter
+import androidx.core.app.NotificationCompat.getColor
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
